af2cea61f25ebd88bca1db0c2d61fada7b6b78b9
shuu <shuu@aura-office.co.jp>
2024-11-13 21:43:47 +0900

    [HTML] ヘッダー => ロゴリンクの削除

--- include/header.php
+++ include/header.php
@@ -1,3 +1,3 @@
-<a href="<?php echo HOME_URL; ?>" id="header" class="header hover_alpha">
+<div id="header" class="header">
   <img class="header__logo" src="<?php echo ASSETS_URL;?>/svg/logo01.svg" alt="みんなの保育 保育士派遣サービス">
-</a>
+</div>
