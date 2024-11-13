0bb993841d61bfdee2f643d6a304f8b834b78c15
shuu <shuu@aura-office.co.jp>
2024-11-13 09:48:26 +0900

    [HTML] お問い合わせ完了 => ページ作成

--- contact/thanks/index.php
+++ contact/thanks/index.php
@@ -23,33 +23,98 @@ $subpageflag = true; #サブページのフラグ
     <?php include(DOCUMENT_ROOT."/include/html_head2.php"); ?>
   </head>
   <body class="<?php echo $pagename; ?> subpage thanks">
-    <?php include(DOCUMENT_ROOT."/include/potential-recruit_remarket_body1.php"); ?>
+    <div id="loader" class="overlay">
+      <div class="loader"></div>
+    </div>
     <?php include(DOCUMENT_ROOT."/include/html_body1.php"); ?>
     <?php include(DOCUMENT_ROOT."/include/header.php"); ?>
+
+    <button type="button" aria-label="ナビゲーションメニュー" class="nav_toggle" id="nav_toggle"><span class="bd"></span></button>
+
+    <?php # Navigation ?>
+    <nav id="navigation" class="navigation">
+      <ul class="navigation__list">
+        <li class="navigation__list-item">
+          <a href="../../#service" class="navigation__list-anchor">サービス</a>
+        </li>
+        <li class="navigation__list-item">
+          <a href="../../#area" class="navigation__list-anchor">お仕事エリア</a>
+        </li>
+        <li class="navigation__list-item">
+          <a href="../../#salary" class="navigation__list-anchor">お給与</a>
+        </li>
+        <li class="navigation__list-item">
+          <a href="../../#voice" class="navigation__list-anchor">ご利用者の声</a>
+        </li>
+        <li class="navigation__list-item">
+          <a href="../../#faq" class="navigation__list-anchor">よくあるご質問</a>
+        </li>
+        <li class="navigation__list-item">
+          <a href="../../#company" class="navigation__list-anchor">運営会社</a>
+        </li>
+        <li class="navigation__list-item">
+          <a href="../../#contact" class="btn3">お問い合わせ</a>
+        </li>
+      </ul>
+    </nav>
+    <?php # /Navigation ?>
+
     <main>
-      <div class="container">
-        <ol class="topicpath">
-          <li class="home"><a href="<?php echo HOME_URL; ?>/" class="hover_line blue">HOME</a></li>
-          <li><a href="<?php echo HOME_URL.'/'.$pagegrp.'/'; ?>"><?php echo $grptitle;?></a></li>
+      <div class="content_page--small">
+        <ol class="topicpath block_content">
+          <li class="home"><a href="<?php echo HOME_URL; ?>/">HOME</a></li>
           <li><?php echo $pagetitle;?></li>
         </ol>
-
-        <h3 class="headline_type1 vari3">お問い合わせありがとうございました。</h3>
-
-        <p class="content_paragraph sp_justify">
-          ご入力されたアドレスに自動返信メールが配信されます。<br>
-          自動返信メールが届かない場合は、<b><?php echo $site_phone;?></b>までお電話ください。
-        </p>
-        <p class="content_paragraph sp_justify">
-          お問い合わせ内容を確認し、担当者より折り返しご連絡いたしますので今しばらくお待ち下さい。<br>
-          その他、何かご不明な点等ございましたら、お気軽にお問い合わせ下さい。
-        </p>
-
-        <a href="<?php echo HOME_URL;?>/" class="btn_type2 mauto">トップへ戻る</a>
       </div>
+
+      <section class="block_section_large ">
+        <div class="bg_gray padding_content_large">
+          <div class="content_page--small">
+            <h2 class="headline1 block_content_large">お問い合わせありがとうございました。</h2>
+
+            <div class="box5">
+              <p class="typography5">
+                ご入力されたアドレスに自動返信メールが配信されます。<br>
+                自動返信メールが届かない場合は、<b><a href="tel:06-6314-6001">06-6314-6001</a></b>までお電話ください。
+              </p>
+              <p class="typography5 block_content">
+                お問い合わせ内容を確認し、担当者より折り返しご連絡いたしますので今しばらくお待ち下さい。<br>
+                その他、何かご不明な点等ございましたら、お気軽にお問い合わせ下さい。
+              </p>
+
+              <div class="flex_type4">
+                <a href="<?php echo HOME_URL;?>/" class="btn1">トップへ戻る</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
 
-    <?php include(DOCUMENT_ROOT."/include/footer.php"); ?>
+    <footer class="footer" id="footer">
+      <div class="content_page">
+        <a href="../../" target="_blank" class="footer__logo hover_alpha">
+          <img src="<?php echo ASSETS_URL;?>/svg/logo01.svg" alt="みんなの保育 保育士派遣サービス">
+        </a>
+
+        <ul class="footer__nav">
+          <li class="footer__nav-item">
+            <a href="https://torisan-net.com/privacy/" target="_blank" class="footer__nav-anchor">プライバシーポリシー</a>
+          </li>
+        </ul>
+      </div>
+
+      <small class="copyright">Copyright © TORISAN CO..LTD All RIGHTS RESERVED.</small>
+    </footer>
+
     <?php include(DOCUMENT_ROOT."/include/html_body2.php"); ?>
+
+    <script>
+      // ロード
+      $(window).on('load', function() {
+        $('#mainimage').addClass('load');
+        $('#loader').fadeOut();
+      });
+    </script>
   </body>
 </html>
